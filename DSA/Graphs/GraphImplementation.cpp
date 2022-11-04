d5ce00c6113372a47d3c49b60a521f3034500986
Param Kansagra <param.j.kansagra@gmail.com>
2022-11-05 00:42:13 +0530

    Initial commit

---
+++ DSA/Graphs/GraphImplementation.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <unordered_map>
+#include <list>
+
+using namespace std;
+
+template <class T>
+class graph{
+    public:
+        unordered_map<T,list<T>> adj;
+
+        // direction is for if the graph is directed or not
+        // int u is the node and v is the new node we are adding
+        void addEdge(T u,T v,bool direction){
+            // direction == 0 means undirected graph
+            // direction == 1 means directed graph
+
+            // create a edge from u to v
+            adj[u].push_back(v);
+
+            // if the graph is undirected then add adj[v] to u
+            if(direction == 0){
+                adj[v].push_back(u);
+            }
+        }
+
+
+        void printAdjList(){
+            for(auto i:adj){
+                cout<<i.first<<"->";
+                for(auto j:i.second){
+                    cout<<j<<",";
+                }cout<<endl;
+            }
+        }
+};
+
+int main(){
+
+    int n;
+    cout<<"Input the number of nodes"<<endl;
+    cin>>n;
+
+    int m;
+    cout<<"Input the number of edges"<<endl; 
+    cin>>m;
+
+    graph<int> g; // create a undirected graph
+
+    for(int i=0;i<m;i++){
+        int u,v; // konsa node se konse node pe jaa raha hai
+        cin>>u>>v;
+
+        g.addEdge(u,v,0); 
+    }
+
+    // print the graph
+    g.printAdjList();
+
+    return 0;
+}
