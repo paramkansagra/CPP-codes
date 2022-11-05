7f29e1c4f5e7c90e792a33ac43505a3582cd8dc8
Param Kansagra <param.j.kansagra@gmail.com>
2022-11-05 11:21:20 +0530

    Initial commit

--- DSA/Graphs/GraphBFS.cpp
+++ DSA/Graphs/GraphBFS.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <unordered_map>
-#include <list>
+#include <set>
 
 using namespace std;
 
@@ -25,7 +25,54 @@ class graph{
         }
 };
 
+#include <bits/stdc++.h> 
 
+void prepAdjList(unordered_map<int,set<int>> &adjList,vector<pair<int,int>> edges){
+    for(auto i:edges){
+        adjList[i.first].insert(i.second);
+        adjList[i.second].insert(i.first);
+    }
+}
+
+void bfs(unordered_map<int,set<int>> adjList,unordered_map<int,bool>& visited,vector<int>& ans, int node){
+    queue<int> q;
+    q.push(node);
+    
+    visited[node] = 1;
+    
+    while(!q.empty()){
+        int frontNode = q.front();
+        q.pop();
+        
+//         store front node in ans
+        ans.push_back(frontNode);
+        
+//         traverse all neighbours of front node
+        for(auto i:adjList[frontNode]){
+            if(!visited[i]){
+                q.push(i);
+                visited[i] = 1;
+            }
+        }
+    }
+}
+
+vector<int> BFS(int vertex, vector<pair<int, int>> edges)
+{
+    unordered_map<int,set<int>> adjList;
+    vector<int> ans;
+    unordered_map<int,bool> visited;
+    
+    prepAdjList(adjList,edges);
+//     traverse all elements of graph
+    for(int i=0;i<vertex;i++){
+        if(!visited[i]){
+            bfs(adjList,visited,ans,i);
+        }
+    }
+    
+    return ans;
+}
 
 int main(){
 
@@ -35,7 +82,7 @@ int main(){
 
     int m;
     cout<<"Input the number of edges"<<endl; 
-    cin>>m;s
+    cin>>m;
 
     graph<int> g; // create a undirected graph
 
