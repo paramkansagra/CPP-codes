57e3074c5a776fd3565d133e817817e8255a8901
Param Kansagra <param.j.kansagra@gmail.com>
2022-11-07 09:24:41 +0530

    Initial commit

--- DSA/Graphs/GraphDFS.cpp
+++ DSA/Graphs/GraphDFS.cpp
@@ -1,5 +1,4 @@
-#include <bits/stdc++.h> 
-
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -24,47 +23,38 @@ class graph{
         }
 };
 
-void prepAdjList(unordered_map<int,set<int>> &adjList,vector<pair<int,int>> edges){
-    for(auto i:edges){
-        adjList[i.first].insert(i.second);
-        adjList[i.second].insert(i.first);
-    }
-}
 
-void bfs(unordered_map<int,set<int>> adjList,unordered_map<int,bool>& visited,vector<int>& ans, int node){
-    queue<int> q;
-    q.push(node);
+void dfs(int node,unordered_map<int,bool>& visited,unordered_map<int,list<int>>& adjList,vector<int>& component){
+    component.push_back(node);
+    visited[node] = true;
     
-    visited[node] = 1;
-    
-    while(!q.empty()){
-        int frontNode = q.front();
-        q.pop();
-        
-//         store front node in ans
-        ans.push_back(frontNode);
-        
-//         traverse all neighbours of front node
-        for(auto i:adjList[frontNode]){
-            if(!visited[i]){
-                q.push(i);
-                visited[i] = 1;
-            }
+//     har connected node ke liye recursive call
+    for(auto i:adjList[node]){
+        if(!visited[i]){
+            dfs(i,visited,adjList,component);
         }
     }
 }
 
-vector<int> BFS(int vertex, vector<pair<int, int>> edges)
+vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
 {
-    unordered_map<int,set<int>> adjList;
-    vector<int> ans;
+    unordered_map<int,list<int>> adjList;
+    for(int i=0;i<edges.size();i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+        
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+    
+    vector<vector<int>> ans;
     unordered_map<int,bool> visited;
     
-    prepAdjList(adjList,edges);
-//     traverse all elements of graph
-    for(int i=0;i<vertex;i++){
+    for(int i=0;i<V;i++){
         if(!visited[i]){
-            bfs(adjList,visited,ans,i);
+            vector<int> component;
+            dfs(i,visited,adjList,component);
+            ans.push_back(component);
         }
     }
     
